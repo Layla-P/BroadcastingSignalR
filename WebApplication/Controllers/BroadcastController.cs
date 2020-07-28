8084450bad4757eb60bfc87f1bb8be0850bf230b
layla-p <layla.porter@gmail.com>
2020-07-28 09:36:07 +0100

    initial commit

---
+++ WebApplication/Controllers/BroadcastController.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/broadcast")]
+    public class BroadcastController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hub;
+        
+        public BroadcastController(IHubContext<ChatHub> hub)
+        {
+            _hub = hub;
+        }
+        
+        [HttpGet]
+        public async Task Get(string user, string message)
+        {
+            await _hub.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
